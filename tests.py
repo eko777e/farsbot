03779dc514a3badf82b4bcc729149f44611fbdd1
eko777e <replitserver445@gmail.com>
2026-01-13 19:58:56 +0400

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,22 @@
+TESTS = [
+    {
+        "test": """
+**Gün sonunun testi!**
+
+Sual • Salam farsca necədir?
+Sual • Kitab farsca nədir?
+Sual • Dost farsca necədir?
+Sual • Ev farsca nədir?
+Sual • Gün farsca nədir?
+""",
+        "answers": """
+**Gün sonunun test cavabları**
+
+Cavab • سلام
+Cavab • کتاب
+Cavab • دوست
+Cavab • خانه
+Cavab • روز
+"""
+    }
+]
