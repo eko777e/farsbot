3e07ce3e47f72c3e113a7c1e6730a0b23e7ce964
eko777e <replitserver445@gmail.com>
2026-01-13 19:58:41 +0400

    Create gram.py

---
+++ gram.py
@@ -0,0 +1,20 @@
+GRAMMAR = [
+    """
+**Gündəlik Qrammatika**
+
+📌 İndiki zaman
+
+من می‌روم — Mən gedirəm  
+تو می‌روی — Sən gedirsən  
+او می‌رود — O gedir
+""",
+    """
+**Gündəlik Qrammatika**
+
+📌 Keçmiş zaman
+
+من رفتم — Mən getdim  
+تو رفتی — Sən getdin  
+او رفت — O getdi
+"""
+]
