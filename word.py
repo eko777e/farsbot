5216516f065439bb075e4ff9c8d6d73d2c2671b0
eko777e <replitserver445@gmail.com>
2026-01-13 22:13:39 +0400

    Update and rename words.py to word.py

---
+++ word.py
@@ -0,0 +1,20 @@
+# word.py
+# Struktur: {"Gün 1": [(fars, tələffüz, tərcümə), ...], "Gün 2": [...]}
+
+daily_words = {
+    "Gün 1": [
+        ("سلام", "salam", "Salam"),
+        ("خوب", "xub", "Yaxşı"),
+        ("دوست", "dust", "Dost"),
+        ("کتاب", "ketab", "Kitab"),
+        ("خانه", "xane", "Ev")
+    ],
+    "Gün 2": [
+        ("مدرسه", "madrese", "Məktəb"),
+        ("معلم", "moalem", "Müəllim"),
+        ("دانشجو", "daneshju", "Tələbə"),
+        ("درس", "dars", "Dərs"),
+        ("قلم", "qalam", "Qələm")
+    ],
+    # Gün 3-30 əlavə et
+}
